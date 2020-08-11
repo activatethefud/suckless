0d0e8a012ae000e34a77e0a3f523c5ac0003866c
Nikola <nikolasutic@protonmail.com>
2020-08-11 12:31:51 +0200

    Fixed fonts for st.

--- st-0.8.3/config.h
+++ st-0.8.3/config.h
@@ -5,8 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-//static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static char *font = "Monospace:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Liberation Mono:size=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
